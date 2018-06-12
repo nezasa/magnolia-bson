ebf325e2d4eae3db6827b20e437ece52fcb32094
João Ferreira <jtjeferreira@gmail.com>
2018-06-12 11:22:56 +0100

    bump magnolia and remove 1 asInstanceOf

--- build.sbt
+++ build.sbt
@@ -4,5 +4,5 @@ version := "0.1"
 
 scalaVersion := "2.11.12"
 
-libraryDependencies += "com.propensive" %% "magnolia" % "0.6.1"
-libraryDependencies += "org.reactivemongo" %% "reactivemongo" % "0.12.6"
+libraryDependencies += "com.propensive" %% "magnolia" % "0.8.0"
+libraryDependencies += "org.reactivemongo" %% "reactivemongo-bson" % "0.12.6"
--- src/main/scala/BSONDerivation.scala
+++ src/main/scala/BSONDerivation.scala
@@ -7,11 +7,9 @@ object BSONDerivation {
   import magnolia._
   import scala.language.experimental.macros
 
-  type BSONValueHandler[T] = BSONHandler[_ <: BSONValue, T]
+  type Typeclass[T] = BSONHandler[_ <: BSONValue, T]
 
-  type Typeclass[T] = BSONValueHandler[T]
-
-  def combine[T](caseClass: CaseClass[BSONValueHandler, T]): BSONValueHandler[T] = {
+  def combine[T](caseClass: CaseClass[Typeclass, T]): Typeclass[T] = {
 
     def write(t: T): BSONDocument =
       BSONDocument(caseClass.parameters.map(p => (p.label, p.typeclass.write(p.dereference(t)))))
@@ -19,8 +17,8 @@ object BSONDerivation {
     def read(bsonValue: BSONValue): T = bsonValue match {
       case doc: BSONDocument =>
         caseClass.construct { p =>
-          doc.getUnflattenedTry(p.label) match {
-            case Success(Some(v)) => p.typeclass.asInstanceOf[BSONHandler[BSONValue, T]].read(v)
+          doc.getAsUnflattenedTry(p.label)(p.typeclass) match {
+            case Success(Some(v)) => v
             case Success(None) => p.default.get // TODO better ex
             case Failure(ex) => throw ex
           }
@@ -32,18 +30,21 @@ object BSONDerivation {
   }
 
   def dispatch[T](sealedTrait: SealedTrait[Typeclass, T]): Typeclass[T] = {
-    def write(t: T): BSONDocument = {
+    def write(t: T): BSONValue = {
       sealedTrait.dispatch(t) { subType =>
-        // any case class should serialize to a BSONDocument, so cast should be okay
-        val doc: BSONDocument = subType.typeclass.write(subType.cast(t)).asInstanceOf[BSONDocument]
-        doc ++ ("className" -> subType.label)
+        subType.typeclass.writeTry(subType.cast(t)) match {
+          case Success(doc@BSONDocument(_)) => doc ++ ("className" -> subType.typeName.full)
+          case Success(v) => v
+          case Failure(ex) => throw ex
+        }
+
       }
     }
 
     def read(bsonValue: BSONValue): T = bsonValue match {
       case doc: BSONDocument =>
         val className = doc.getAs[String]("className").getOrElse(throw new IllegalStateException("'className' is required for sealed traits"))
-        val subtype = sealedTrait.subtypes.find(_.label == className).get
+        val subtype = sealedTrait.subtypes.find(_.typeName.full == className).get
         subtype.typeclass.asInstanceOf[BSONHandler[BSONValue, T]].read(doc)
       case _ => throw new IllegalStateException("we only handle-case classes")
     }
@@ -51,5 +52,5 @@ object BSONDerivation {
     BSONHandler(read, write)
   }
 
-  implicit def gen[T]: Typeclass[T] = macro Magnolia.gen[T]
+  implicit def gen[T]: BSONHandler[_ <: BSONValue, T] = macro Magnolia.gen[T]
 }
